ccf1f3afc5c26244b2a256f3a64683cac174fbf8
Luciana Acevedo <luacevedo90@gmail.com>
2016-09-08 23:38:26 -0300

    Enabled mLab db

--- server/models/db.js
+++ server/models/db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-// mongoose.connect('mongodb://luacevedo:luli1990@ds019816.mlab.com:19816/heroku_mnp4jp47');
-mongoose.connect('mongodb://localhost/mochi-test2');
+mongoose.connect('mongodb://luacevedo:luli1990@ds019816.mlab.com:19816/heroku_mnp4jp47');
+// mongoose.connect('mongodb://localhost/mochi-test2');
