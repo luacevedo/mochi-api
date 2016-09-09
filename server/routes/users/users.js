9cea02ceae457217fdd4e91dc8e50116120a4619
Luciana Acevedo <luacevedo90@gmail.com>
2016-09-08 23:33:59 -0300

    Commented unnecesary code

--- server/routes/users/users.js
+++ server/routes/users/users.js
@@ -2,18 +2,18 @@ var _ = require('underscore');
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose'); //mongo connection
-var bodyParser = require('body-parser'); //parses information from POST
-var methodOverride = require('method-override'); //used to manipulate POST
+// var bodyParser = require('body-parser'); //parses information from POST
+// var methodOverride = require('method-override'); //used to manipulate POST
 var UserTest = require('../../models/UserTest');
 
-router.use(bodyParser.urlencoded({extended: true}))
-router.use(methodOverride(function (req, res) {
-    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-        var method = req.body._method;
-        delete req.body._method;
-        return method
-    }
-}));
+// router.use(bodyParser.urlencoded({extended: true}))
+// router.use(methodOverride(function (req, res) {
+//     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+//         var method = req.body._method;
+//         delete req.body._method;
+//         return method
+//     }
+// }));
 
 router.route('/')
     .get(function (req, res, next) {
